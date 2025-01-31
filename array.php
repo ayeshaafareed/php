8e23ba9dc2a01e2d9fdc9aa678038426f4f6e5af
ayeshaafareed <aa.fareed87@gmail.com>
2025-01-31 11:52:26 +0500

    Add files via upload

---
+++ array.php
@@ -0,0 +1,47 @@
+<?php
+// Indexed Arrays  
+//  $data = array("ahmed","zara","454545","6767");
+//  echo $data[3] 
+
+
+
+// Associative Arrays
+
+$data = array(
+    array("name"=>"sana","email"=>"sana@gamil.com","phone"=>"5454545"),
+    array("saba",454464,"saba@gmail.com")
+
+);
+
+echo $data[0]["name"];
+
+
+// multidimentional Array
+
+// $data = array(
+// "sara" => array("phone"=>"4654646","email"=>"sara@gmail.com","status"=>"housewife"),
+// "ayesha" => array("phone"=>"46545456","email"=>"ayesha@gmail.com","status"=>"developer")
+
+
+
+// );
+// echo $data["sara"]["email"];
+// echo "<br>";
+// echo $data["ayesha"]["email"]
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Arrays</h1>
+</body>
+</html>
